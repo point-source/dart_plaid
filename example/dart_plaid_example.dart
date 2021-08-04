6c5a78cc6e6dbcdf4f76ad9862499c676cb94939
PointSource <47544779+point-source@users.noreply.github.com>
2021-08-04 16:00:18 -0700

    Fix example

--- example/dart_plaid_example.dart
+++ example/dart_plaid_example.dart
@@ -1,5 +1,46 @@
+import 'package:chopper/chopper.dart';
 import 'package:dart_plaid/dart_plaid.dart';
 
-void main() {
-  var plaid = Plaid.create();
+Future<void> main() async {
+  /// Checks for errors in an api response and throws if found
+  checkResponse(Response response) {
+    if (!response.isSuccessful) {
+      throw Exception('${response.statusCode}: ${response.base.reasonPhrase}');
+    }
+  }
+
+  // Create an instance of the Plaid service using a clientId and secret
+  PlaidService plaid = Plaid.create('my-client-id', 'my-secret',
+      environment: PlaidEnvironment.sandbox);
+
+  // Create a new institution instance and get the public token
+  final publicTokenRes = await plaid.sandboxPublicTokenCreate(
+      body: SandboxPublicTokenCreateRequest(
+          institutionId: 'ins_10', initialProducts: [Products.assets]));
+
+  // Check for errors in the response
+  checkResponse(publicTokenRes);
+
+  final publicToken = publicTokenRes.body?.publicToken;
+
+  // Exchange public token for private access token
+  final accessTokenRes = await plaid.itemPublicTokenExchange(
+      body: ItemPublicTokenExchangeRequest(publicToken: publicToken));
+
+  // Check for errors in the response
+  checkResponse(accessTokenRes);
+
+  final accessToken = accessTokenRes.body?.accessToken;
+
+  // Use the access token to get a list of accounts
+  final accountsRes = await plaid.accountsGet(
+      body: AccountsGetRequest(accessToken: accessToken));
+
+  // Check for errors in the response
+  checkResponse(accountsRes);
+
+  // Print out the accounts
+  for (AccountBase a in accountsRes.body?.accounts ?? []) {
+    print(a.officialName);
+  }
 }
--- lib/dart_plaid.dart
+++ lib/dart_plaid.dart
@@ -1,3 +1,6 @@
 library dart_plaid;
 
 export 'package:dart_plaid/src/plaid.dart';
+export 'package:dart_plaid/generated/client_index.dart';
+export 'package:dart_plaid/generated/client_mapping.dart';
+export 'package:dart_plaid/generated/plaid_service.swagger.dart';
